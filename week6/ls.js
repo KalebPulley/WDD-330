1eb7c5b8392dd5a9521e5af68a6d1b597bd1e00d
KalebPulley <pul21001@byui.edu>
2022-03-28 15:39:36 -0600

    push

--- week6/ls.js
+++ week6/ls.js
@@ -1,77 +1,81 @@
 const TODO_LIST = "todoList0";
 
 function getTodoList() {
-let todoListString = localStorage.getItem(TODO_LIST);
+  let todoListString = localStorage.getItem(TODO_LIST);
 
-let todoList = [];
+  let todoList = [];
 
-if (todoListString){
+  if (todoListString) {
     todoList = JSON.parse(todoListString);
+  }
 
-}
-
-return todoList;
+  return todoList;
 }
 
 function saveTodo(todo) {
-//get curant list
-let todoList = getTodoList();
+  //get curant list
+  let todoList = getTodoList();
 
-//add new item
-todoList.push(todo);
+  //add new item
+  todoList.push(todo);
 
-//save item
-localStorage.setItem(TODO_LIST, JSON.stringify(todoList));
-//debug msg
-console.log(`saved $todo`);
+  //save item
+  localStorage.setItem(TODO_LIST, JSON.stringify(todoList));
+  //debug msg
+  console.log(`saved $todo`);
 }
 
 function deleteTodo(id) {
-//get curant list
-let todoList = getTodoList();
+  //get curant list
+  let todoList = getTodoList();
 
-//find item
-let newList = todoList.filter(todo => todo.id != id);
+  //find item
+  let newList = todoList.filter((todo) => todo.id != id);
 
-//save to storage
-localStorage.setItem(TODO_LIST, JSON.stringify(newList));
+  //save to storage
+  localStorage.setItem(TODO_LIST, JSON.stringify(newList));
 
-//debug msg
-console.log(`deleted $todo`);
+  //debug msg
+  console.log(`deleted $todo`);
 }
 
 function togleTodo(id) {
-     //get curant list
-    let todoList = getTodoList();
-    
-    //find item
-    let newList = [];
-    todoList.forEach(todo => {if ( todo.id == id){
-        if (todo.completed == true){
-            todo.completed = false;
-            newList.push(todo);
-        }else{
-            todo.completed = true;
-            newList.push(todo);
-        }
-    }else {newList.push(todo);}});
-    
-    //save to storage
-    localStorage.setItem(TODO_LIST, JSON.stringify(newList));
-    
-    /*/debug msg
-    console.log(`deleted $todo`);
-    } */}
-function deleteAll() {
-    let todoList = [];
+  //get curant list
+  let todoList = getTodoList();
 
-    localStorage.setItem(TODO_LIST, JSON.stringify(todoList));
+  //find item
+  let newList = [];
+  todoList.forEach((todo) => {
+    if (todo.id == id) {
+      if (todo.completed == true) {
+        todo.completed = false;
+        newList.push(todo);
+      } else {
+        todo.completed = true;
+        newList.push(todo);
+      }
+    } else {
+      newList.push(todo);
+    }
+  });
+
+  //save to storage
+  localStorage.setItem(TODO_LIST, JSON.stringify(newList));
+
+  /*/debug msg
+    console.log(`deleted $todo`);
+    } */
+}
+function deleteAll() {
+  let todoList = [];
+
+  localStorage.setItem(TODO_LIST, JSON.stringify(todoList));
 }
 
 export default {
-    saveTodo,
-    deleteTodo,
-    getTodoList,
-    deleteAll,
-    togleTodo
-}
+  saveTodo,
+  deleteTodo,
+  getTodoList,
+  deleteAll,
+  togleTodo,
+};
